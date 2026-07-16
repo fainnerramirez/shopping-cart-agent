c51bce6698f4b9eca3cb112f2b999805ff3b138a
fainnerramirez <fainnerramirez@gmail.com>
2026-07-16 11:59:26 -0500

    feat: se agrega navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,11 +2,11 @@ import ButtonGithub from "../ui/button-github";
 
 export default function Navbar() {
     return (
-        <div className="w-[90%] m-auto justify-center items-center mb-14 mt-5 neumorfismo-forward p-1">
-            <div className="flex items-center justify-center">
+        <div className="w-[90%] m-auto mb-14 mt-5 neumorfismo-forward p-1">
+            <div className="flex items-center justify-around">
                 <ButtonGithub />
-                <div className="flex items-center w-[100%] m-auto text-gray-600">
-                    <input className="neumorfismo-input" placeholder="buscar producto" />
+                <div className="text-gray-800">
+                    <input className="neumorfismo-input w-[600px]!" placeholder="buscar producto" />
                 </div>
             </div>
         </div>
